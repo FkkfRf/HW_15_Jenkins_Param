facf3abe0ae50e22fecb72708fee433dc4253d36
FkkfRf <Fkkf.Rf@gmail.com>
2022-11-05 00:54:21 +0300

    add Configurations in branch Properties

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,43 +1,62 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import static io.qameta.allure.Allure.step;
+
 import helpers.Attach;
 
 public class BaseTest {
 
     @BeforeAll
     static void setUp() {
+        String browserName = System.getProperty("browser", "chrome")
+        String browserVer = System.getProperty("browserVersion", "100");
+        String browserSize = System.getProperty("browserSize", "1920x1500");
+        String remoteUrl = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         step("Устанавливаем интеграцию с Selenide", () -> {
             SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         });
-
-
         step("Устанавливаем конфигурацию", () -> {
             DesiredCapabilities capabilities = new DesiredCapabilities();
- //           capabilities.setCapability("browserName", "edge");
-//        capabilities.setCapability("browserVersion", "100.0");
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
         });
         step("Открываем тестируемую форму в заданной конфигурации браузера", () -> {
             Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = "1920x1080";
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            Configuration.browser = browserName;
+            Configuration.browserVersion = browserVer;
+            Configuration.browserSize = browserSize;
+            Configuration.remote = remoteUrl;
+            Configuration.holdBrowserOpen = true;
+            if (remoteUrl != null) {
+                Configuration.remote = remoteUrl;
+            }
         });
     }
 
     @AfterEach
     void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
+        step("прикрепляем логи и отчеты выполнения", () -> {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+        });
+    }
+
+    @AfterAll
+    void CloseWebDriver() {
+        step("закрываем вебдрайвер", () -> {
+            Selenide.closeWebDriver();
+        });
     }
 
 }
